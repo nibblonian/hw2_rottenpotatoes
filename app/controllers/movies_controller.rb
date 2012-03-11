476c32f62e2af4b2f32e975acb4fcd73209da8d1
Ted Rust <ted@rusts.net>
2012-03-11 06:52:41 +0000

    Final tweaks for part 3 of homework 2.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-  	sort = params[:sort] || :id
-    @movies = Movie.all(:order => sort)
+  	@sort = params[:sort] || :id
+    @movies = Movie.all(:order => @sort)
   end
 
   def new
